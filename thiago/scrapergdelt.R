01ab2564863566955bd7cd8e28e1d4ca4c7061b5
Aleksander Dietrichson <dietrichson@gmail.com>
2023-09-25 21:01:00 +0000

    Clase de hoy

--- thiago/scrapergdelt.R
+++ thiago/scrapergdelt.R
@@ -1,8 +1,14 @@
 library(readr)
 source(here::here("R","scrape-news.R"))
-mydata <- read_csv("data/-20230201-20230301.csv")
+
+mydata <- read_csv(here::here("data","-20230201-20230301.csv")) # Actualizar con las 11800
+
 my_links <- mydata$URL
-purrr::map(my_links[1:2],~{
+purrr::map(my_links ,~{
   scrape_news(.x,title_tag = "title",content_tag = "div.StoryTextContainer" )
 }) %>% 
   bind_rows()-> my_data
+
+output_filename <- here::here("data", "scraping-clarin-2023-09-25.rds") #actualizar cuando lo hagas
+
+readr::write_rds(my_data, output_filename)
