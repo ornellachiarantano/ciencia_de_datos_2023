e9bd7cbdb80a8f72d831fbe89ced592d7a066a76
Thiago Donato <thiagovdonato@gmail.com>
2023-10-02 19:40:02 +0000

    noseqponer

--- thiago/scrapergdelt.R
+++ thiago/scrapergdelt.R
@@ -1,8 +1,8 @@
 library(readr)
 source(here::here("R","scrape-news.R"))
-mydata <- read_csv("data/-20230201-20230301.csv")
+mydata <- read_csv("data/clarin-20230201-20230815.csv")
 my_links <- mydata$URL
-purrr::map(my_links[1:2],~{
+purrr::map(my_links,~{
   scrape_news(.x,title_tag = "title",content_tag = "div.StoryTextContainer" )
 }) %>% 
   bind_rows()-> my_data
